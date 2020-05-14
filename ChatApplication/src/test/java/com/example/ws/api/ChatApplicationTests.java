fecfb71dfcd01ff3b6eb1b74dcd678bc8ebf992f
Emir Totic <emirtotic@gmail.com>
2020-05-14 23:19:05 +0200

    Chat Application using Java websocket.

---
+++ ChatApplication/src/test/java/com/example/ws/api/ChatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.ws.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	
+	  @Test void contextLoads() { 
+		  
+	  }
+	 
+
+}
